0422e977e31fc74a2871161d2e1aa6bc7a9ae972
hannah <huilee9999@gmail.com>
2020-03-09 11:14:30 -0400

    update State component

---
+++ lib/chapter8/8.1_PointerEvent.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(PointerWidget());
+
+class PointerWidget extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    throw UnimplementedError();
+  }
+
+}
+
